2e3ab0c89d6c99fb52fac79c68a92d8e3bcaffa6
ajravikumar <ajayravikumar007@gmail.com>
2020-06-03 14:53:19 +0100

    Dataloading

--- attention.py
+++ attention.py
@@ -1,1 +1,69 @@
+ 
+import shutil
+import os
+import time
+from datetime import datetime
+import argparse
+import numpy as np
+from tqdm import tqdm
 
+import torch
+import torch.nn as nn
+import torch.optim as optim
+from torch.autograd import Variable
+from torchsample.transforms import RandomRotate, RandomTranslate, RandomFlip, ToTensor, Compose, RandomAffine
+from torchvision import transforms
+import torch.nn.functional as F
+from tensorboardX import SummaryWriter
+
+from dataloader import MRDataset
+import model
+
+from sklearn import metrics
+
+
+def run(args):
+
+
+	augmentor = Compose([
+        transforms.Lambda(lambda x: torch.Tensor(x)),
+        RandomRotate(25),
+        RandomTranslate([0.11, 0.11]),
+        RandomFlip(),
+        transforms.Lambda(lambda x: x.repeat(3, 1, 1, 1).permute(1, 0, 2, 3)),
+    ])
+
+	train_dataset = MRDataset('./data/', args.task, args.plane, transform=augmentor, train=True)
+	array, label, weight = train_dataset[0]
+	print(array.shape, label)
+	train_loader = torch.utils.data.DataLoader( train_dataset, batch_size=1, shuffle=True, num_workers=11, drop_last=False)
+
+	validation_dataset = MRDataset('./data/', args.task, args.plane, train=False)
+	validation_loader = torch.utils.data.DataLoader( validation_dataset, batch_size=1, shuffle=-True, num_workers=11, drop_last=False)
+	
+	
+
+def parse_arguments():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-t', '--task', type=str, required=True,
+                        choices=['abnormal', 'acl', 'meniscus'])
+    parser.add_argument('-p', '--plane', type=str, required=True,
+                        choices=['sagittal', 'coronal', 'axial'])
+    parser.add_argument('--prefix_name', type=str, required=True)
+    parser.add_argument('--augment', type=int, choices=[0, 1], default=1)
+    parser.add_argument('--lr_scheduler', type=str,
+                        default='plateau', choices=['plateau', 'step'])
+    parser.add_argument('--gamma', type=float, default=0.5)
+    parser.add_argument('--epochs', type=int, default=50)
+    parser.add_argument('--lr', type=float, default=1e-5)
+    parser.add_argument('--flush_history', type=int, choices=[0, 1], default=0)
+    parser.add_argument('--save_model', type=int, choices=[0, 1], default=1)
+    parser.add_argument('--patience', type=int, default=5)
+    parser.add_argument('--log_every', type=int, default=100)
+    args = parser.parse_args()
+    return args
+
+
+if __name__ == "__main__":
+    args = parse_arguments()
+    run(args)
